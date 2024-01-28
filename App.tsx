c1c0621a404deb1373fd088dced713c6ecfc4d8f
Stanisław Synowiec <stasyn1410@gmail.com>
2024-01-28 18:22:24 +0100

    core: add prettier

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View className='flex justify-center items-center flex-1'>
-      <Text className='text-red-500 font-bold'>Welcome!</Text>
-      <TouchableOpacity onPress={() => console.log('test')} className='bg-purple-600 px-5 py-2 rounded-lg text-white'><Text>START</Text></TouchableOpacity>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View className="flex justify-center items-center flex-1">
+			<Text className="text-red-500 font-bold">Welcome!</Text>
+			<TouchableOpacity
+				onPress={() => console.log('test')}
+				className="bg-purple-600 px-5 py-2 rounded-lg text-white"
+			>
+				<Text>START</Text>
+			</TouchableOpacity>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
