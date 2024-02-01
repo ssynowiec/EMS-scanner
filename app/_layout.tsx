441dfda0f54e83ba2fb50634497d8abf844065d6
Stanisław Synowiec <stasyn1410@gmail.com>
2024-02-01 20:25:03 +0100

    core: add tanstack query provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,10 @@ import { useState } from 'react';
 import { Drawer } from 'expo-router/drawer';
 import { NoInternetConnectionScreen } from '../src/screens/noInternetConnection';
 import { StatusBar } from 'expo-status-bar';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ConnectionStatusBar } from 'react-native-ui-lib';
+
+const queryClient = new QueryClient();
 
 const MainLayout = () => {
 	const [isInternetConnection, setIsInternetConnection] = useState<boolean>();
@@ -13,30 +17,44 @@ const MainLayout = () => {
 
 	return (
 		<>
-			<Drawer>
-				<Drawer.Screen
-					name="index"
-					options={{
-						drawerLabel: 'Home',
-						title: 'Start',
-					}}
+			<QueryClientProvider client={queryClient}>
+				<ConnectionStatusBar
+					onConnectionChange={(isConnected) =>
+						setIsInternetConnection(isConnected)
+					}
 				/>
-				<Drawer.Screen
-					name="welcome/index"
-					options={{
-						drawerLabel: 'Welcome',
-						title: 'Welcome',
-					}}
-				/>
-				<Drawer.Screen
-					name="scan/index"
-					options={{
-						drawerLabel: 'Scan',
-						title: 'Scan',
-					}}
-				/>
-			</Drawer>
-			<StatusBar style="auto" hidden={true} />
+				<Drawer>
+					<Drawer.Screen
+						name="index"
+						options={{
+							drawerLabel: 'Home',
+							title: 'Start',
+						}}
+					/>
+					<Drawer.Screen
+						name="welcome/index"
+						options={{
+							drawerLabel: 'Welcome',
+							title: 'Welcome',
+						}}
+					/>
+					<Drawer.Screen
+						name="scan/index"
+						options={{
+							drawerLabel: 'Scan',
+							title: 'Scan',
+						}}
+					/>
+					<Drawer.Screen
+						name="tickets/index"
+						options={{
+							drawerLabel: 'All tickets',
+							title: 'All tickets',
+						}}
+					/>
+				</Drawer>
+				<StatusBar style="auto" hidden={true} />
+			</QueryClientProvider>
 		</>
 	);
 };
