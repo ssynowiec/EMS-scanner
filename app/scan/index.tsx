1f187bdc4f822ae319b60287e1db4a18f58495f8
Stanisław Synowiec <stasyn1410@gmail.com>
2024-02-04 11:49:18 +0100

    add: validate ticket by scan code

--- app/scan/index.tsx
+++ app/scan/index.tsx
@@ -1,12 +1,39 @@
+import { useMutation } from '@tanstack/react-query';
+import { useState } from 'react';
 import { View } from 'react-native';
 import { Button } from 'react-native-ui-lib';
-import Text from 'react-native-ui-lib/text';
+
+import { CameraScreen } from '../../src/components/cameraScreen/cameraScreen';
+import { validateTicketByNumber } from '../../src/utils/validateTicketByNumber';
 
 const ScanPage = () => {
+	const [startScanner, setStartScanner] = useState(false);
+
+	const onSuccessScanned = useMutation({
+		mutationFn: async (data: string) => {
+			await validateTicketByNumber(
+				'61772e63-4345-4e60-91c9-db7e1901e993',
+				data,
+			);
+		},
+	});
+
+	if (startScanner) {
+		return (
+			<CameraScreen
+				setStartCamera={setStartScanner}
+				onCodeScanned={onSuccessScanned.mutate}
+			/>
+		);
+	}
+
 	return (
 		<View className="flex flex-1 justify-center items-center">
-			<Text>Scan Page</Text>
-			<Button label="START SCAN" />
+			<Button
+				label="OPEN SCANNER"
+				onPress={() => setStartScanner(true)}
+				className="rounded-md"
+			/>
 		</View>
 	);
 };
