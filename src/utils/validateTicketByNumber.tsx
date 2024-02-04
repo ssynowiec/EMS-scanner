ebdd9c10038140ead063464b3985b5c0cfb9e14e
Stanisław Synowiec <stasyn1410@gmail.com>
2024-02-04 15:59:56 +0100

    fix: validate Ticket function

--- src/utils/validateTicketByNumber.tsx
+++ src/utils/validateTicketByNumber.tsx
@@ -5,11 +5,5 @@ export const validateTicketByNumber = async (
 	const res = await fetch(
 		`${process.env.EXPO_PUBLIC_API_URL}ticket/validate/${eventID}/${ticketNo}`,
 	);
-	const data = await res.json();
-
-	if (res.ok) {
-		return data;
-	}
-
-	throw new Error(data.message);
+	return await res.json();
 };
