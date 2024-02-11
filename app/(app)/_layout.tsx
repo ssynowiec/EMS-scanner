c1a8b304a9e16833ed79069e68048379476e0eac
Stanisław Synowiec <stasyn1410@gmail.com>
2024-02-11 14:57:56 +0100

    move/fix: query provider, no internet status & status bar

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,26 +1,12 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Redirect } from 'expo-router';
 import { Drawer } from 'expo-router/drawer';
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
 import { Text } from 'react-native';
-import { ConnectionStatusBar } from 'react-native-ui-lib';
 
 import { CustomDrawerContent } from '../../src/components/customDrawerContent/customDrawerContent';
 import { useSession } from '../../src/contexts/auth/authContext';
-import { NoInternetConnectionScreen } from '../../src/screens/noInternetConnection';
-
-const queryClient = new QueryClient();
 
 const MainLayout = () => {
 	const { session, isLoading } = useSession();
-
-	const [isInternetConnection, setIsInternetConnection] = useState<boolean>();
-
-	if (!isInternetConnection)
-		return (
-			<NoInternetConnectionScreen setConnection={setIsInternetConnection} />
-		);
 
 	if (isLoading) return <Text>Loading...</Text>;
 
@@ -28,44 +14,36 @@ const MainLayout = () => {
 
 	return (
 		<>
-			<QueryClientProvider client={queryClient}>
-				<ConnectionStatusBar
-					onConnectionChange={(isConnected) =>
-						setIsInternetConnection(isConnected)
-					}
+			<Drawer drawerContent={(props) => <CustomDrawerContent {...props} />}>
+				<Drawer.Screen
+					name="index"
+					options={{
+						drawerLabel: 'Home',
+						title: 'Start',
+					}}
 				/>
-				<Drawer drawerContent={(props) => <CustomDrawerContent {...props} />}>
-					<Drawer.Screen
-						name="index"
-						options={{
-							drawerLabel: 'Home',
-							title: 'Start',
-						}}
-					/>
-					<Drawer.Screen
-						name="welcome/index"
-						options={{
-							drawerLabel: 'Welcome',
-							title: 'Welcome',
-						}}
-					/>
-					<Drawer.Screen
-						name="scan/index"
-						options={{
-							drawerLabel: 'Scan',
-							title: 'Scan',
-						}}
-					/>
-					<Drawer.Screen
-						name="tickets/index"
-						options={{
-							drawerLabel: 'All tickets',
-							title: 'All tickets',
-						}}
-					/>
-				</Drawer>
-				<StatusBar style="auto" hidden />
-			</QueryClientProvider>
+				<Drawer.Screen
+					name="welcome/index"
+					options={{
+						drawerLabel: 'Welcome',
+						title: 'Welcome',
+					}}
+				/>
+				<Drawer.Screen
+					name="scan/index"
+					options={{
+						drawerLabel: 'Scan',
+						title: 'Scan',
+					}}
+				/>
+				<Drawer.Screen
+					name="tickets/index"
+					options={{
+						drawerLabel: 'All tickets',
+						title: 'All tickets',
+					}}
+				/>
+			</Drawer>
 		</>
 	);
 };
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,34 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Slot } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { ConnectionStatusBar } from 'react-native-ui-lib';
 
 import { SessionProvider } from '../src/contexts/auth/authContext';
+import { NoInternetConnectionScreen } from '../src/screens/noInternetConnection';
+
+const queryClient = new QueryClient();
 
 const RootLayout = () => {
+	const [isInternetConnection, setIsInternetConnection] = useState<boolean>();
+
+	if (!isInternetConnection)
+		return (
+			<NoInternetConnectionScreen setConnection={setIsInternetConnection} />
+		);
+
 	return (
-		<SessionProvider>
-			<Slot />
-		</SessionProvider>
+		<QueryClientProvider client={queryClient}>
+			<ConnectionStatusBar
+				onConnectionChange={(isConnected) =>
+					setIsInternetConnection(isConnected)
+				}
+			/>
+			<SessionProvider>
+				<Slot />
+			</SessionProvider>
+			<StatusBar style="auto" hidden />
+		</QueryClientProvider>
 	);
 };
 
