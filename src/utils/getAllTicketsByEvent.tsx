c6f5a63cc4e3842339676f4bc08afef0085362ba
Stanisław Synowiec <stasyn1410@gmail.com>
2024-03-06 18:11:05 +0100

    add: no-cache

--- src/utils/getAllTicketsByEvent.tsx
+++ src/utils/getAllTicketsByEvent.tsx
@@ -1,4 +1,10 @@
 export const getAllTicketsByEvent = async (eventId: string) => {
-	const res = await fetch(`${process.env.EXPO_PUBLIC_API_URL}ticket/all`);
+	const res = await fetch(`${process.env.EXPO_PUBLIC_API_URL}ticket/all`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		cache: 'no-cache',
+	});
 	return await res.json();
 };
