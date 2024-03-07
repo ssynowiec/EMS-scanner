02927f7a4a920bae5cc481f5942689f82a1250a9
Stanisław Synowiec <stasyn1410@gmail.com>
2024-03-07 10:35:08 +0100

    fix: fet tickets by event id

--- src/utils/getAllTicketsByEvent.tsx
+++ src/utils/getAllTicketsByEvent.tsx
@@ -1,10 +1,13 @@
 export const getAllTicketsByEvent = async (eventId: string) => {
-	const res = await fetch(`${process.env.EXPO_PUBLIC_API_URL}ticket/all`, {
-		method: 'GET',
-		headers: {
-			'Content-Type': 'application/json',
+	const res = await fetch(
+		`${process.env.EXPO_PUBLIC_API_URL}ticket/all/${eventId}`,
+		{
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			cache: 'no-cache',
 		},
-		cache: 'no-cache',
-	});
+	);
 	return await res.json();
 };
